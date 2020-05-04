ddb0d83398a3a0e22044918a886456bceb4d4b99
Crowdbotics <team@crowdbotics.com>
2020-05-04 21:51:34 +0000

    Update models

--- backend/pain_barn_16607/wsgi.py
+++ backend/pain_barn_16607/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pain_barn_16607.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pain_barn_16607.settings")
 
 application = get_wsgi_application()
